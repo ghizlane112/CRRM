83ef1fce2a8590a1c6c69f3b422c55cf35510ad2
ghizlane <bayaghizla@gmail.com>
2024-09-05 13:04:04 +0100

    ajoute de campagns dans lead models

--- Cenv/Projet/lead/models.py
+++ Cenv/Projet/lead/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 #from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
-#
+from campaigns.models import CompanyPublicitaire
 
 User = get_user_model()
 
@@ -10,32 +10,30 @@ User = get_user_model()
 
 # Create your models here.
 class Lead(models.Model):
-    STATUTS=[
-      ('Nouveau','Nouveau'),
-      ('Contacte','Contacte'),
-      ('Qualifie','Qualifie'),
-      ('Converti','Converti'),
-      ('Perdu','Perdu')
-
+    STATUTS = [
+        ('Nouveau', 'Nouveau'),
+        ('Contacte', 'Contacte'),
+        ('Qualifie', 'Qualifie'),
+        ('Converti', 'Converti'),
+        ('Perdu', 'Perdu')
     ]
-    id=models.AutoField(primary_key=True)
-    nom=models.CharField(max_length=20)
-    prenom=models.CharField(max_length=20)
-    email=models.EmailField(unique=True)
-    telephone=models.CharField(max_length=10)
-    source=models.CharField(max_length=100)
-    statut=models.CharField(max_length=50,choices=STATUTS,default='Nouveau')
-    note=models.TextField(blank=True,null=True)
-    date_creation=models.DateTimeField(auto_now_add=True)
+    
+    id = models.AutoField(primary_key=True)
+    nom = models.CharField(max_length=20)
+    prenom = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    telephone = models.CharField(max_length=10)
+    source = models.CharField(max_length=100)
+    statut = models.CharField(max_length=50, choices=STATUTS, default='Nouveau')
+    note = models.TextField(blank=True, null=True)
+    date_creation = models.DateTimeField(auto_now_add=True)
     responsable = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='leads')
     is_deleted = models.BooleanField(default=False)
     deleted_at = models.DateTimeField(null=True, blank=True)
-    #campaign = models.ForeignKey(CompanyPublicitaire, on_delete=models.SET_NULL, null=True, blank=True, related_name='leads')
-    
+    campaign = models.ForeignKey(CompanyPublicitaire, on_delete=models.SET_NULL, null=True, blank=True, related_name='lead_campaigns')  # Changed related_name
     
     def __str__(self):
         return f"{self.prenom} {self.nom}"
-    
 
     def get_campaign(self):
         from campaigns.models import CompanyPublicitaire  # Import local
