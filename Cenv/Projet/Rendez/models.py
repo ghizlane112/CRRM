9cb3715843151872ec22fb7f8f72dba25cccf164
ghizlane <bayaghizla@gmail.com>
2024-09-08 15:56:27 +0100

    chagement poue essai de effectue la sipprission mais il ne fonctionne pas

--- Cenv/Projet/Rendez/models.py
+++ Cenv/Projet/Rendez/models.py
@@ -23,8 +23,14 @@ class Event(models.Model):
 
 
 class History(models.Model):
-    event = models.ForeignKey(Event,on_delete=models.CASCADE,null=True, blank=True)
-    action = models.CharField(max_length=50)  # e.g., 'delete'
+    ACTION_CHOICES = [
+        ('add', 'Add'),
+        ('update', 'Update'),
+        ('delete', 'Delete'),
+    ]
+
+    event = models.ForeignKey(Event,on_delete=models.CASCADE)
+    action = models.CharField(max_length=10, choices=ACTION_CHOICES)
     reason = models.TextField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)  # Enregistrement de l'utilisateur
     timestamp = models.DateTimeField(auto_now_add=True)
